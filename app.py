67913942ceb265d9b3954eec6dd50f173a5d120c
launchpencil <66775280+launchpencil@users.noreply.github.com>
2024-05-16 08:14:57 +0000

    o

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ def count_insects(image):
     # グレースケールに変換
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # 画像の平滑化（ガウシアンブラー）
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # 適応的閾値処理（パラメータ調整）
-    binary = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 7)
+    binary = cv2.adaptiveThreshold(140, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 7)
 
     st.image(binary, caption='閾値処理された画像', use_column_width=True)
 
