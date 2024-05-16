4ea0acdae20165f0db81da333de196868d33646d
launchpencil <66775280+launchpencil@users.noreply.github.com>
2024-05-16 09:30:11 +0000

    a

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import numpy as np
 def count_insects(image, min_contour_area=200):
     # グレースケールに変換
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+
     # 閾値処理を追加
     _, binary = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY_INV)
-    
+
     # 輪郭を抽出
     contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
-    
+
     # 輪郭を描画
     result_image = image.copy()
     insect_count = 0
-    
+
     # 境界ボックスを計算し、重複するボックスを結合
     bounding_boxes = []
     for contour in contours:
@@ -23,19 +23,13 @@ def count_insects(image, min_contour_area=200):
         if min_contour_area < area:
             x, y, w, h = cv2.boundingRect(contour)
             bounding_boxes.append((x, y, x + w, y + h))
-    
+
     # 重複するボックスを結合
     merged_boxes = merge_boxes(bounding_boxes)
-    
+
     # 結合されたボックスを描画
     for box in merged_boxes:
         x1, y1, x2, y2 = box
-        # 長方形のサイズを少し小さくする
-        margin = 10  # 任意のマージンを設定
-        x1 += margin
-        y1 += margin
-        x2 -= margin
-        y2 -= margin
         cv2.rectangle(result_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(result_image, f'{x2 - x1}x{y2 - y1}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         insect_count += 1
